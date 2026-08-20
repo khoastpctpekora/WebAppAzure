9c625b0173cfb041031416d64546d37335ec1851
NguyenNhoAnhKhoa <105312661@student.swin.edu.au>
2026-08-21 01:25:49 +1000

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppAzure.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
